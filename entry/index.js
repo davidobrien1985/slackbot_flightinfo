4de88c520bc1b6358a4fd2f33938755efc3377dc
David O'Brien <me@david-obrien.net>
2016-08-23 20:43:21 +1000

    fix now?

--- entry/index.js
+++ entry/index.js
@@ -1,29 +1,29 @@
 module.exports = function (context, req) {
 
-var options = {
-    hostname: "https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${req.query.icao}",
-    port: 443,
-    method: 'Get',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    }
-};
+    var options = {
+        hostname: "https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${req.query.icao}",
+        port: 443,
+        method: 'Get',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    };
 
-var req = http.request(options, (res) => {
-    context.log(`STATUS: ${res.statusCode}`);
-    context.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-        console.log(`BODY: ${chunk}`);
+    var request = http.request(options, (res) => {
+        context.log(`STATUS: ${res.statusCode}`);
+        context.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            context.log('No more data in response.');
+        });
     });
-    res.on('end', () => {
-        context.log('No more data in response.');
+
+    request.on('error', (e) => {
+        context.log(`problem with request: ${e.message}`);
     });
-});
 
-req.on('error', (e) => {
-    context.log(`problem with request: ${e.message}`);
-});
-
-context.done();
+    context.done();
 };
