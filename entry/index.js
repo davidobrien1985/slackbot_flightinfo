d1f802b786eecddb5b176ed69b980bb5d80e9599
David O'Brien <me@david-obrien.net>
2016-08-23 22:28:47 +1000

    strip back to basics

--- entry/index.js
+++ entry/index.js
@@ -10,22 +10,9 @@ module.exports = function (context, req) {
 
     function getMetar(icaocode) {
 
+        console.log(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}`);
         return http.get({
             host: `https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}`,
-        }, function (response) {
-            // Continuously update stream with data
-            var body = '';
-            response.on('data', function (d) {
-                body += d;
-            });
-            response.on('end', function () {
-
-                // Data reception is done, do whatever with it!
-                var parsed = JSON.parse(body);
-                output({
-                    body: parsed
-                });
-            });
         });
     }
 
