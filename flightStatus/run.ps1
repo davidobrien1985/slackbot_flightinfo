953a6b2b9859ea231a7ab75c886cdae1a43f4280
David O'Brien <me@david-obrien.net>
2016-08-25 19:44:45 +1000

    add error handling

--- flightStatus/run.ps1
+++ flightStatus/run.ps1
@@ -28,40 +28,47 @@ $Headers = @{
 }
 
 $flightInfoEx = Invoke-RestMethod -Method Get -Uri "https://flightxml.flightaware.com/json/FlightXML2/FlightInfoEx?ident=$($request)&howMany=2" -Headers $Headers -Verbose
-$actualflightInfo = $flightInfoEx.FlightInfoExResult.flights[0]
-$actualflightInfo
-
-$flightInfo = Invoke-RestMethod -Method Get -Uri "https://flightxml.flightaware.com/json/FlightXML2/AirlineFlightInfo?faFlightID=$($actualflightInfo.faFlightID)" -Headers $Headers -Verbose
-$flightInfo.AirlineFlightInfoResult
-(ConvertFrom-Unixdate $actualflightInfo.filed_departuretime).toString()
-
-
-$decoded_response_url = [System.Web.HttpUtility]::UrlDecode(($in.Split('&')[9]).Split('=')[1]) 
-$decoded_response_url
-
-$result = @{
-  'Flight #' = $flightInfo.AirlineFlightInfoResult.ident
-  'From' = $actualflightInfo.originCity
-  'To' = $actualflightInfo.destinationCity
-  'Type of aircraft' = $actualflightInfo.aircrafttype
-  'Departure Terminal' = $flightInfo.AirlineFlightInfoResult.terminal_orig
-  'Departure Gate' = $flightInfo.AirlineFlightInfoResult.gate_orig
-  'Arrival Terminal' = $flightInfo.AirlineFlightInfoResult.terminal_dest
-  'Arrival Gate' = if ($flightInfo.AirlineFlightInfoResult.gate_dest) {$flightInfo.AirlineFlightInfoResult.gate_dest} else {'n/a'}
-  'Filed Departure Time' = (Get-LocalTime -UTCTime (ConvertFrom-Unixdate $actualflightInfo.filed_departuretime).toString()).toString()
-  'Estimated Arrival Time' = (Get-LocalTime -UTCTime (ConvertFrom-Unixdate $actualflightInfo.estimatedarrivaltime).toString()).toString()
-}
-
-if ($flightInfo.AirlineFlightInfoResult) {
-    $response_body = @{
-        text = "$($result | Convertto-Json)"
-        response_type = 'in_channel'
-    }
+if ($flightInfoEx.error) {
+	$response_body = @{
+			text = 'This flight number does not exist or does not exist in the Flightaware database.'
+		}
 }
 else {
-    $response_body = @{
-        text = 'Something went wrong with the Flight Status API.'
-    }
+	$actualflightInfo = $flightInfoEx.FlightInfoExResult.flights[0]
+	$actualflightInfo
+
+	$flightInfo = Invoke-RestMethod -Method Get -Uri "https://flightxml.flightaware.com/json/FlightXML2/AirlineFlightInfo?faFlightID=$($actualflightInfo.faFlightID)" -Headers $Headers -Verbose
+	$flightInfo.AirlineFlightInfoResult
+	(ConvertFrom-Unixdate $actualflightInfo.filed_departuretime).toString()
+
+
+	$decoded_response_url = [System.Web.HttpUtility]::UrlDecode(($in.Split('&')[9]).Split('=')[1]) 
+	$decoded_response_url
+
+	$result = @{
+	  'Flight #' = $flightInfo.AirlineFlightInfoResult.ident
+	  'From' = $actualflightInfo.originCity
+	  'To' = $actualflightInfo.destinationCity
+	  'Type of aircraft' = $actualflightInfo.aircrafttype
+	  'Departure Terminal' = $flightInfo.AirlineFlightInfoResult.terminal_orig
+	  'Departure Gate' = $flightInfo.AirlineFlightInfoResult.gate_orig
+	  'Arrival Terminal' = $flightInfo.AirlineFlightInfoResult.terminal_dest
+	  'Arrival Gate' = if ($flightInfo.AirlineFlightInfoResult.gate_dest) {$flightInfo.AirlineFlightInfoResult.gate_dest} else {'n/a'}
+	  'Filed Departure Time' = (Get-LocalTime -UTCTime (ConvertFrom-Unixdate $actualflightInfo.filed_departuretime).toString()).toString()
+	  'Estimated Arrival Time' = (Get-LocalTime -UTCTime (ConvertFrom-Unixdate $actualflightInfo.estimatedarrivaltime).toString()).toString()
+	}
+
+	if ($flightInfo.AirlineFlightInfoResult) {
+		$response_body = @{
+			text = "$($result | Convertto-Json)"
+			response_type = 'in_channel'
+		}
+	}
+	else {
+		$response_body = @{
+			text = 'Something went wrong with the Flight Status API.'
+		}
+	}
 }
 
 Invoke-RestMethod -Uri $decoded_response_url -Method Post -ContentType 'application/json' -Body (ConvertTo-Json $response_body) -Verbose
