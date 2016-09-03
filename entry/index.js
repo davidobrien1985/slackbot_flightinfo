c09fae7686f76c452de9d2bcf803af524ca2e8bd
David O'Brien <me@david-obrien.net>
2016-09-04 00:14:25 +1000

    Move getMetar function outside of if block, add output.

--- entry/index.js
+++ entry/index.js
@@ -7,34 +7,36 @@ module.exports = function (context, req) {
     var callback = input.split('&')[9].split('=')[1];
     var username = input.split('&')[6].split('=')[1];
 
-    context.log('Input was %s',userquery);
+    function getMetar(icaocode) {
+
+        context.log(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}&callback=${callback}`);
+
+        https.get(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}&callback=${callback}`, function (res) {
+            var body = ''; // Will contain the final response
+            // Received data is a buffer.
+            // Adding it to our body
+            res.on('data', function (data) {
+                body += data;
+            });
+            // After the response is completed, parse it and log it to the console
+            res.on('end', function () {
+                var parsed = JSON.parse(body);
+                context.log(parsed);
+            });
+        })
+            // If any error has occured, log error to console
+            .on('error', function (e) {
+                context.log("Got error: " + e.message);
+            });
+    }
+
+    context.log('Input was %s', userquery);
+    context.log('%s', userquery.length);
+    context.log('%s', userquery.match('/^[a-zA-Z]+$'));
 
     if ((userquery.length == 3 || userquery.length == 4) && userquery.match('/^[a-zA-Z]+$')) {
 
         context.bindings.response = `Hello ${username}, I am getting your weather for ${userquery}, try again if you have not heard back in 20s.`;
-
-        function getMetar(icaocode) {
-
-            context.log(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}&callback=${callback}`);
-
-            https.get(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}&callback=${callback}`, function (res) {
-                var body = ''; // Will contain the final response
-                // Received data is a buffer.
-                // Adding it to our body
-                res.on('data', function (data) {
-                    body += data;
-                });
-                // After the response is completed, parse it and log it to the console
-                res.on('end', function () {
-                    var parsed = JSON.parse(body);
-                    context.log(parsed);
-                });
-            })
-                // If any error has occured, log error to console
-                .on('error', function (e) {
-                    context.log("Got error: " + e.message);
-                });
-        }
 
         getMetar(userquery);
 
