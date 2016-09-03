c565c883537e5e76ae098bd098bb17e6b793baf1
David O'Brien <me@david-obrien.net>
2016-09-04 00:07:35 +1000

    Prepare entry function for multiple function forks.

--- entry/index.js
+++ entry/index.js
@@ -3,38 +3,41 @@ module.exports = function (context, req) {
     var https = require('https');
 
     var input = JSON.stringify(req.body);
-    var icao = input.split('&')[8].split('=')[1];
+    var userquery = input.split('&')[8].split('=')[1];
     var callback = input.split('&')[9].split('=')[1];
     var username = input.split('&')[6].split('=')[1];
 
-    context.log('Input was %s',icao);
+    context.log('Input was %s',userquery);
 
-    context.bindings.response = `Hello ${username}, I am getting your weather for ${icao}, try again if you have not heard back in 20s.`;
+    if ((userquery.length == 3 || userquery.length == 4) && userquery.match('/^[a-zA-Z]+$')) {
 
-    function getMetar(icaocode) {
+        context.bindings.response = `Hello ${username}, I am getting your weather for ${userquery}, try again if you have not heard back in 20s.`;
 
-        context.log(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}&callback=${callback}`);
+        function getMetar(icaocode) {
 
-        https.get(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}&callback=${callback}`, function (res) {
-            var body = ''; // Will contain the final response
-            // Received data is a buffer.
-            // Adding it to our body
-            res.on('data', function (data) {
-                body += data;
-            });
-            // After the response is completed, parse it and log it to the console
-            res.on('end', function () {
-                var parsed = JSON.parse(body);
-                context.log(parsed);
-            });
-        })
-            // If any error has occured, log error to console
-            .on('error', function (e) {
-                context.log("Got error: " + e.message);
-            });
-    }
+            context.log(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}&callback=${callback}`);
 
-    getMetar(icao);
+            https.get(`https://dogithub.azurewebsites.net/api/metarSlackbot?icao=${icaocode}&callback=${callback}`, function (res) {
+                var body = ''; // Will contain the final response
+                // Received data is a buffer.
+                // Adding it to our body
+                res.on('data', function (data) {
+                    body += data;
+                });
+                // After the response is completed, parse it and log it to the console
+                res.on('end', function () {
+                    var parsed = JSON.parse(body);
+                    context.log(parsed);
+                });
+            })
+                // If any error has occured, log error to console
+                .on('error', function (e) {
+                    context.log("Got error: " + e.message);
+                });
+        }
 
-    context.done();
+        getMetar(userquery);
+
+        context.done();
+    };
 };
