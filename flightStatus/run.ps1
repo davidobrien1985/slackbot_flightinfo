ba4757e93220e748fc422a203f82881d73849674
David O'Brien <me@david-obrien.net>
2016-09-10 00:28:47 +1000

    did I fix it?

--- flightStatus/run.ps1
+++ flightStatus/run.ps1
@@ -28,10 +28,6 @@ $flightno = $req_query_flightnumber.Substring(3)
 
 $flight = Invoke-RestMethod -Method Get -Uri "https://flightxml.flightaware.com/json/FlightXML2/AirlineFlightSchedules?startDate=$($today)&endDate=$($tomorrow)&airline=$($airline)&flightno=$($flightno)" -Headers $Headers -Verbose
 
-Write-Output "https://flightxml.flightaware.com/json/FlightXML2/AirlineFlightSchedules?startDate=$($today)&endDate=$($tomorrow)&airline=$($airline)&flightno=$($flightno)"
-$flight.AirlineFlightSchedulesResult.data
-
-
 if ($flight.error) {
 	$response_body = @{
 			text = 'This flight number does not exist or does not exist in the Flightaware database.'
@@ -44,25 +40,20 @@ $flightident = "$req_query_flightnumber@$($actualflight.departuretime)"
 $flightInfoEx = (Invoke-RestMethod -Method Get -Uri "https://flightxml.flightaware.com/json/FlightXML2/FlightInfoEx?ident=$($flightident)&howMany=2" -Headers $Headers -Verbose).FlightInfoExResult.flights
 $airlineflightInfo = (Invoke-RestMethod -Method Get -Uri "https://flightxml.flightaware.com/json/FlightXML2/AirlineFlightInfo?faFlightID=$($flightInfoEx.faFlightID)" -Headers $Headers -Verbose).AirlineFlightInfoResult
 
-$actualflight
-$flightInfoEx
-$airlineflightInfo
-${actualflight}.ident
-${actualflight}.aircrafttype
 
 $result = @"
 Flight # = $(${actualflight}.ident)
-Code Share Flight # = $(if (${actualflight.actual_ident}) {${actualflight.actual_ident}} else {'n/a'})
-From = ${flightInfoEx.FlightInfoExResult.flights.originName}
-To = ${flightInfoEx.FlightInfoExResult.flights.destinationName}
-Type of aircraft = ${actualflight.aircrafttype}
-Filed Departure Time = $((Get-LocalTime -UTCTime ((ConvertFrom-Unixdate ${actualflight.departuretime}).ToString())).ToString())
-Estimated Arrival Time = $((Get-LocalTime -UTCTime ((ConvertFrom-Unixdate ${actualflight.arrivaltime}).ToString())).ToString())
-Departure Gate = ${airlineflightInfo.gate_orig}
-Departure Terminal = ${airlineflightInfo.terminal_orig}
+Code Share Flight # = $(if ($(${actualflight}.actual_ident)) {$(${actualflight}.actual_ident)} else {'n/a'})
+From = $(${flightInfoEx}.FlightInfoExResult.flights.originName)
+To = $(${flightInfoEx}.FlightInfoExResult.flights.destinationName)
+Type of aircraft = $(${actualflight}.aircrafttype)
+Filed Departure Time = $((Get-LocalTime -UTCTime ((ConvertFrom-Unixdate $(${actualflight}.departuretime)).ToString())).ToString())
+Estimated Arrival Time = $((Get-LocalTime -UTCTime ((ConvertFrom-Unixdate $(${actualflight}.arrivaltime)).ToString())).ToString())
+Departure Gate = $(${airlineflightInfo}.gate_orig)
+Departure Terminal = $(${airlineflightInfo}.terminal_orig)
 "@
 
-$result.ToString()
+
 $response_body = @{
 	text = "$result"
 	response_type = 'in_channel'
