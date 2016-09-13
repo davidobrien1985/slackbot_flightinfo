35a3ac1016e294f7b0f5b7c281f68d71b324356e
David O'Brien <me@david-obrien.net>
2016-09-14 00:14:46 +1000

    more var fixes

--- metarslackbot/run.ps1
+++ metarslackbot/run.ps1
@@ -43,12 +43,12 @@ $airport = (Invoke-RestMethod -Method Get -Uri "https://flightxml.flightaware.co
 
 # Get air pressure in hPa
 $regex = 'Q\d{4}'
-($weather.MetarExResult.metar.raw_data) -match $regex
+($weather.raw_data) -match $regex
 $pressure = $Matches[0]
 
 # CAVOK?
 $regex = 'CAVOK'
-if (($weather.MetarExResult.metar.raw_data) -match $regex) {
+if (($weather.raw_data) -match $regex) {
 	$CAVOK = $true
 }
 
