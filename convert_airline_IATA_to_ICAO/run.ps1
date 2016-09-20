8d1b0e15d4afcf6bd5cae4ef05a9510d4be8db00
David O'Brien <me@david-obrien.net>
2016-09-20 23:49:29 +1000

    Use different database for IATA code.

--- convert_airline_IATA_to_ICAO/run.ps1
+++ convert_airline_IATA_to_ICAO/run.ps1
@@ -1,13 +1,13 @@
 
 $req_query_iata
 
-$Fields = @{"iatacode" = "$req_query_iata"}
-$WebResponse = Invoke-RestMethod -Uri "http://www.airlinecodes.co.uk/airlcoderes.asp" -Method Post -Body $Fields -Verbose
+# status Y means current data
+$Fields = @{"status" = "Y"; "iataairl" = "$req_query_iata"}
+$WebResponse = Invoke-RestMethod -Uri "http://avcodes.co.uk/airlcoderes.asp" -Method Post -Body $Fields -Verbose
 
-$regex = '<TD>\D{3}<\/TD>'
+$regex = 'ICAO Code:<br />&nbsp;\D{3}'
 
 $WebResponse -match $regex
-
 $result = $Matches[0]
 
-Out-File -Encoding Ascii $res -inputObject $result.Substring(4,3)
+Out-File -Encoding Ascii $res -inputObject $result.Split(';')[1]
