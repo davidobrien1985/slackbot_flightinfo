5ce397890de3f4b49190b796b6c611b904420976
David O'Brien <me@david-obrien.net>
2016-09-20 22:23:53 +1000

    Even more output.

--- convert_airline_IATA_to_ICAO/run.ps1
+++ convert_airline_IATA_to_ICAO/run.ps1
@@ -4,6 +4,7 @@ $req_query_iata
 $Fields = @{"iatacode" = "$req_query_iata"}
 $WebResponse = Invoke-RestMethod -Uri "http://www.airlinecodes.co.uk/airlcoderes.asp" -Method Post -Body $Fields
 $TD = $WebResponse.AllElements | Where {$_.TagName -eq "TD" }
+$TD
 
 $regex = '<TD>\D{3}<\/TD>'
 
