5f6a8aa9f83834050d773e687e79f672a159d3ad
David O'Brien <me@david-obrien.net>
2016-09-20 22:39:15 +1000

    Fix output parsing.

--- convert_airline_IATA_to_ICAO/run.ps1
+++ convert_airline_IATA_to_ICAO/run.ps1
@@ -3,16 +3,11 @@ $req_query_iata
 
 $Fields = @{"iatacode" = "$req_query_iata"}
 $WebResponse = Invoke-RestMethod -Uri "http://www.airlinecodes.co.uk/airlcoderes.asp" -Method Post -Body $Fields -Verbose
-$WebResponse
-$TD = $WebResponse.AllElements | Where {$_.TagName -eq "TD" }
-$TD
 
 $regex = '<TD>\D{3}<\/TD>'
 
-foreach ($i in $TD.outerHTML) {
-  $i -match $regex
-}
-$Matches
+$WebResponse -match $regex
+
 $result = $Matches[0]
 
 Out-File -Encoding Ascii $res -inputObject $result.Substring(4,3)
