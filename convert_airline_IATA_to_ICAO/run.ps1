5aad5d39dea7383f9d642ef7268543b86fc8a1e7
David O'Brien <me@david-obrien.net>
2016-09-20 22:08:49 +1000

    Add function to convert airline IATA to ICAO.

---
+++ convert_airline_IATA_to_ICAO/run.ps1
@@ -0,0 +1,15 @@
+
+
+$Fields = @{"iatacode" = "$req_query_iata"}
+$WebResponse = Invoke-WebRequest -Uri "http://www.airlinecodes.co.uk/airlcoderes.asp" -Method Post -Body $Fields
+$TD = $WebResponse.AllElements | Where {$_.TagName -eq "TD" }
+
+$regex = '<TD>\D{3}<\/TD>'
+
+foreach ($i in $TD.outerHTML) {
+  $i -match $regex
+}
+
+$result = $Matches[0]
+
+Out-File -Encoding Ascii $res -inputObject $result.Substring(4,3)
