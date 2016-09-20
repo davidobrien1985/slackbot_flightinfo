dcefa39229e6d4975ec9e4f3d3106e7653454c91
David O'Brien <me@david-obrien.net>
2016-09-20 19:23:46 +1000

    Add user name to metar function.

--- metarslackbot/run.ps1
+++ metarslackbot/run.ps1
@@ -49,21 +49,21 @@ $pressure = ($Matches[0]).SubString(1)
 # CAVOK?
 $regex = 'CAVOK'
 if (($weather.raw_data) -match $regex) {
-	$CAVOK = $true
+  $CAVOK = $true
 }
 
 $result = @"
-*Weather Info for $(${airport}.name) / ${airport_code}*
+* ${username} here is your Weather Info for $(${airport}.name) / ${airport_code}*
     Observation Date = *$((Get-LocalTime -UTCTime ((ConvertFrom-Unixdate $(${weather}.time)).ToString())).ToString())*
-	Clouds = $(${weather}.cloud_friendly)
-	Clouds altitude = $(if ($CAVOK) {"No clouds below 10,000ft"} else {"$(${weather}.cloud_altitude) ft"})
-	Cloud type = $(${weather}.cloud_type)
-	Temperature = $(${weather}.temp_air) C
-	Pressure = $(${pressure}) hPa
-	Wind = $(${weather}.wind_direction) degrees / $(${weather}.wind_speed) kts
-	Wind Gusts = $(${weather}.wind_speed_gust) kts
-	Visibility = $(${weather}.visibility) m
-	Raw Report = $(${weather}.raw_data)
+  Clouds = $(${weather}.cloud_friendly)
+  Clouds altitude = $(if ($CAVOK) {"No clouds below 10,000ft"} else {"$(${weather}.cloud_altitude) ft"})
+  Cloud type = $(${weather}.cloud_type)
+  Temperature = $(${weather}.temp_air) C
+  Pressure = $(${pressure}) hPa
+  Wind = $(${weather}.wind_direction) degrees / $(${weather}.wind_speed) kts
+  Wind Gusts = $(${weather}.wind_speed_gust) kts
+  Visibility = $(${weather}.visibility) m
+  Raw Report = $(${weather}.raw_data)
 "@
 
 if ($result) {
